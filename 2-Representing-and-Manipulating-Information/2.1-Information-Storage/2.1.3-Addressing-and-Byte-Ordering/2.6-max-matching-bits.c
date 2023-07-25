eaa7a96065cb2493c5a76c8b2c6be68b974b0525
Quan Tong <quantonganh@gmail.com>
2023-07-25 14:56:04 +0700

    2.6 max matching bits

---
+++ 2-Representing-and-Manipulating-Information/2.1-Information-Storage/2.1.3-Addressing-and-Byte-Ordering/2.6-max-matching-bits.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+    for (int i = 0; i < len; i++) {
+        printf(" %.2x", start[i]);
+    }
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+int main() {
+    show_int(2607352);
+    show_float(3510593.0);
+
+    return 0;
+}
